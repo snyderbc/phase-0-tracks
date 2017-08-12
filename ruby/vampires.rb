d0a08f5b389a732094feb2344c0f6714e31fb965
Brittany Snyder <snyderbc@gmail.com>
2017-08-12 11:14:09 -0500

    Complete release 3 - process multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,37 +1,44 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp
+until employees == 0
 
-puts "What year were you born?"
-birth_year = gets.chomp
+  puts "What is your name?"
+  name = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance? (y/n)"
-insurance = gets.chomp
+  puts "What year were you born?"
+  birth_year = gets.chomp
 
-if age.to_i == (2017 - birth_year.to_i || 2016 - birth_year.to_i) && (garlic == "y" || insurance == "y")
-  vampire = "Probably not a vampire."
+  puts "Our company cafeteria serves garlic bread.  Should we order some for you? (y/n)"
+  garlic = gets.chomp
+
+  puts "Would you like to enroll in the company’s   health insurance? (y/n)"
+  insurance = gets.chomp
+
+  if age.to_i == (2017 - birth_year.to_i || 2016 -  birth_year.to_i) && (garlic == "y" || insurance ==  "y")
+    vampire = "Probably not a vampire."
+  end
+
+  if age.to_i != (2017 - birth_year.to_i || 2016 -  birth_year.to_i) && (garlic == "n" || insurance ==  "n")
+    vampire = "Probably a vampire."
+  end
+
+  if age.to_i != (2017 - birth_year.to_i || 2016 -  birth_year.to_i) && garlic == "n" && insurance ==  "n"
+    vampire = "Almost certainly a vampire."
+  end
+
+  if name == "Drake Cula" || name == "Tu Fang"
+    vampire = "Definitely a vampire."
+  end
+
+  if vampire != "Probably not a vampire." && vampire != "Probably a vampire." && vampire != "Almost   certainly a vampire." && vampire != "Definitely a   vampire."
+    vampire = "Results inconclusive."
+  end
+
+  p vampire
+
+  employees = employees - 1
 end
-
-if age.to_i != (2017 - birth_year.to_i || 2016 - birth_year.to_i) && (garlic == "n" || insurance == "n")
-  vampire = "Probably a vampire."
-end
-
-if age.to_i != (2017 - birth_year.to_i || 2016 - birth_year.to_i) && garlic == "n" && insurance == "n"
-  vampire = "Almost certainly a vampire."
-end
-
-if name == "Drake Cula" || name == "Tu Fang"
-  vampire = "Definitely a vampire."
-end
-
-if vampire != "Probably not a vampire." && vampire != "Probably a vampire." && vampire != "Almost certainly a vampire." && vampire != "Definitely a vampire."
-  vampire = "Results inconclusive."
-end
-
-p vampire
-
