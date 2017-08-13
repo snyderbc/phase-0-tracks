852aa592158eae10a2a9cc9bfb72eb77b997fc0c
Brittany Snyder <snyderbc@gmail.com>
2017-08-13 14:05:06 -0500

    Add results inconclusive default and second condition - probably a vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,10 +13,15 @@ garlic = gets.chomp
 puts "Would you like to enroll in the company’s health insurance?"
 health_insurance = gets.chomp
 
-vampire = ""
+vampire = "Results inconclusive."
 
   if reported_age == (2017 - birth_year || 2016 - birth_year) && (garlic == "y" || health_insurance == "y")
     vampire = "Probably not a vampire."
   end
+
+ if reported_age != (2017 - birth_year || 2016 - birth_year) && garlic == "n" && health_insurance == "n"
+    vampire = "Almost certainly a vampire."
+  end
+
 
 p vampire
