bde9563edbc279eaeeb0184f1d11171d525d5c10
Brittany Snyder <snyderbc@gmail.com>
2017-08-13 10:13:54 -0500

    Add data type conversions and print data

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,15 +1,18 @@
 puts "Hamster's name?"
 name = gets.chomp
+p name
 
 puts "How loud on scale of 1 to 10?"
-volume = gets.chomp
+volume = gets.chomp.to_i
+p volume
 
 puts "Fur color?"
 fur_color = gets.chomp
+p fur_color
 
 puts "Is the hampster a good candidate (y/n)?"
 candidate = gets.chomp
-
+p candidate
 
 puts "Estimated age?"
 age = gets.chomp
@@ -17,5 +20,7 @@ age = gets.chomp
 if age == ""
   age = nil
 else
-  age = age
+  age = age.to_i
 end
+
+p age
