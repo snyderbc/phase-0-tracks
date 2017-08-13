15a6bb34052f5576249c815d4a85eebe286ec475
Brittany Snyder <snyderbc@gmail.com>
2017-08-13 14:01:04 -0500

    Create first condition - probably not a vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,10 +2,10 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+reported_age = gets.chomp.to_i
 
 puts "What year were you born?"
-birth_year = gets.chomp
+birth_year = gets.chomp.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic = gets.chomp
@@ -13,3 +13,10 @@ garlic = gets.chomp
 puts "Would you like to enroll in the company’s health insurance?"
 health_insurance = gets.chomp
 
+vampire = ""
+
+  if reported_age == (2017 - birth_year || 2016 - birth_year) && (garlic == "y" || health_insurance == "y")
+    vampire = "Probably not a vampire."
+  end
+
+p vampire
