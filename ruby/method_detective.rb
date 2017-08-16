f673ecb129eaadd6f17fcf04e7a5a7476a58a2dd
Brittany Snyder <snyderbc@gmail.com>
2017-08-15 22:40:26 -0500

    Complete first half of method detective

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,10 +4,11 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+# => "InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1, 'o')
+"zom".insert(2, 'o')
 # => “zoom”
 
 # "enhance".<???>
@@ -16,13 +17,15 @@
 # "Stop! You’re under arrest!".<???>
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
-
-# " suspects".<???>
+"the usual" + " suspects"
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# " suspects".<???>
+" suspects".replace "the usual suspects"
+# => "the usual suspects"
+
+"The case of the disappearing last letter".chomp
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
@@ -31,9 +34,9 @@
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+"z".ord
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
