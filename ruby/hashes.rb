db6f944d5a278d79a0c7ae033351a299a3b0fb39
Brittany Snyder <snyderbc@gmail.com>
2017-08-17 20:08:59 -0500

    Store respones in a hash, print hash correclty, and delete if statement for red question

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,38 +1,29 @@
+#Create a hash to store all of their input
+
+client_details = {}
+
 #Ask the client multiple questions (name, age, # of children, decor theme, etc.) and gather their responses. Be sure to use a mix of string, integer, and boolean data.
 
 # Convert any user input to the appropriate data type
 
 puts "What is your name?"
-name = gets.chomp
-puts name
+client_details["name"] = gets.chomp
 
 puts "What is your age?"
-age = gets.chomp.to_i
-puts age
+client_details["age"] = gets.chomp.to_i
+
 
 puts "How many children do you have?"
-children = gets.chomp.to_i
-puts children
+client_details["children"] = gets.chomp.to_i
 
 puts "What decor theme do you prefer?"
-decor_theme = gets.chomp
-puts decor_theme
+client_details["decor"] = gets.chomp
 
 puts "Do you like the color red? (y/n)"
-red = gets.chomp
-
-if red == "y"
-  red = true
-else
-  red = false
-end
-
-puts red
-
-
-#Create a hash to store all of their input
+client_details["red"] = gets.chomp
 
 
 #Print the hash once all questions are complete
 
+p client_details
 
