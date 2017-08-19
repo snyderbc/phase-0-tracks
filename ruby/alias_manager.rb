3e871b619bef1fb90dd6235efaf3ba4a36b94d7e
Brittany Snyder <snyderbc@gmail.com>
2017-08-19 14:23:15 -0500

    Swap first and last name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,8 +4,11 @@ full_name = "Felicia Torres"
 
 #Separate the first name from the last name, and swap them
 
-p full_name.split(' ')
+p full_name = "Felicia Torres".split(' ')
 p full_name.class
+
+name_swap = [full_name[1], full_name[0]]
+p name_swap
 
 #Identify if any of the characters in the word are a vowel. If they are, advance to the next vowel ('aeiou').
 #Identify if any of the characters in the word are a consonant. If they are, advance to the next consonant ('bcdfghjklmnpqrstvwxyz').
