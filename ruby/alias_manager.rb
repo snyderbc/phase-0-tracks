a4390ceafd1662ecd41f62c14f808f3135eeaff4
Brittany Snyder <snyderbc@gmail.com>
2017-08-19 14:15:11 -0500

    Initial commit, turn full name into an arry

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,13 @@
+#Take a full name (first and last) - will be input as a string.
+
+full_name = "Felicia Torres"
+
+#Separate the first name from the last name, and swap them
+
+p full_name.split(' ')
+p full_name.class
+
+#Identify if any of the characters in the word are a vowel. If they are, advance to the next vowel ('aeiou').
+#Identify if any of the characters in the word are a consonant. If they are, advance to the next consonant ('bcdfghjklmnpqrstvwxyz').
+#Return the fake name
+
