fd44e8f0c5585401ffc098809fb6627fc7afcb63
Brittany Snyder <snyderbc@gmail.com>
2017-08-19 00:04:05 -0500

    Complete release 1 using each and map methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,36 @@ def say_goodbye
 end
 
 say_goodbye {|number1, number2| puts "Here's a little help: #{number1} + #{number2}"}
+
+############
+
+dogs = ["dalmation", "lab", "pit bull"]
+
+musicians = {
+  "Led Zeppelin" => "Robert Plant",
+  "Pink Floyd" =>"David Gilmour",
+  "The Beatles" =>"George Harrison"
+}
+
+p dogs
+
+dogs.each do |breed|
+  puts "#{breed} is a type of dog!"
+end
+
+p dogs
+
+dogs.map! do |breed|
+  puts breed
+  breed.upcase
+end
+
+p dogs
+
+p musicians
+
+musicians.each do |band, member|
+  puts "#{member} was in #{band}."
+end
+
+p musicians
