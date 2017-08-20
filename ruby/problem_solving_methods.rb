ee123a5c050dfdc81501a8dd5f1eb6ebf0c2a516
Brittany Snyder <snyderbc@gmail.com>
2017-08-20 16:25:19 -0500

    Complete Release 0, fix to print only the final result

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,24 +1,25 @@
-arr = [42, 89, 23, 1]
+#Look to see if a provided integer is in a provided array
+#For each number in the array, check to see if it is equal to the integer
+#If it is equal, return its location in the array
+#If it is not, return nil
 
+
+arr = [42, 89, 23, 1]
 
 def search_array (array, integer)
     times = 0
+    match_location = ""
       array.each do |number|
         if number == integer
             match_location = times
-            p match_location
         else
             match_location = nil
         end
       times += 1
       end
+    match_location
 end
 
 p search_array(arr, 1)
 
-#Look to see if a provided integer is in a provided array
-#For each number in the array, check to see if it is equal to the integer
-#If it is equal, return its location in the array
-#If it is not, return nil
-#If it is, return the index of the integer
-#If it is not, return nil
+
