8363b77f9261bcec8abdfa552e784e0289da78c2
Brittany Snyder <snyderbc@gmail.com>
2017-08-20 17:02:15 -0500

    Add method for Fibonacci sequence, release 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,5 @@
+###Release 0: Simple Search
+
 #Look to see if a provided integer is in a provided array
 #For each number in the array, check to see if it is equal to the integer
 #If it is equal, return its location in the array
@@ -22,4 +24,20 @@ end
 
 p search_array(arr, 1)
 
+###Release 1: Fibonacci Numbers
 
+def fib(quantity)
+  fib_array = [0, 1]
+  fib_number = 0
+    (quantity - 2).times {|index| fib_array << fib_array[index] + fib_array[index + 1]}
+  p fib_array
+end
+
+fib(100)
+
+
+
+
+#Create an array that already has 0 and 1 stored. Then, for each number entered, add it to the previous number and store it in an array.
+
+#create a method that starts at zero, then
