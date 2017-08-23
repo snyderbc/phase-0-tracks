b20b88f7f647ea530a28a1b79654f6d3c9fc63a2
Brittany Snyder <snyderbc@gmail.com>
2017-08-22 21:59:26 -0500

    Complete Release 2, make 50 instances of class and store in data structure

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -46,3 +46,57 @@ p lab.roll_over == "*roll over*"
 p lab.dog_years(2) == 14
 
 p lab.sit == "*sit*"
+
+
+
+class Fruit
+
+  def initialize(sound)
+    puts sound
+  end
+
+  def rots(days)
+    days.downto(1) do |count_down|
+      puts count_down
+    end
+    p "Rotten!"
+  end
+
+  def grow(name)
+    puts name.downcase
+    puts name.upcase
+  end
+
+end
+
+
+
+# 50.times do |num|
+#   num.to_sym = Fruit.new("crunch")
+# end
+
+# 50.times do |num|
+#   num_string = num.to_s
+#   num_string= Fruit.new("crunch")
+# end
+
+fruit_list = {}
+
+50.times do |num|
+  fruit_list[num] = Fruit.new("splat")
+  end
+p fruit_list
+
+p fruit_list[49.to_s]
+
+fruit_list.each do |num_name, a_fruit|
+    a_fruit.rots(num_name + 1)
+    a_fruit.grow(num_name.to_s + "apple")
+  end
+
+
+  50.times do |num|
+  Fruit.new("splat")
+  end
+
+  p fruit_list
