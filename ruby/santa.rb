0056d687b4362ac07affb225e88ae80058af488f
Brittany Snyder <snyderbc@gmail.com>
2017-08-25 16:29:16 -0500

    Complete release 1, initialize several diverse instances of Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,7 +5,7 @@ class Santa
   end
 
   def eat_milk_and_cookies(cookie_type)
-    puts "That was a good #{cookie_type}!"
+    puts "That was a good #{cookie_type} cookie!"
   end
 
   def initialize(gender, ethnicity)
@@ -18,8 +18,22 @@ class Santa
 
 end
 
-test = Santa.new
 
-test.speak
-test.eat_milk_and_cookies("chocolate chip")
+########DRIVER CODE###############
+
+#Create several instances of Santa class, with diverse genders and ethnicities
+santas = []
+santa_genders = ["genderless", "pangender", "female", "queer", "male"]
+santa_ethnicities = ["hispanic", "white", "black", "native american", "asian"]
+
+santa_genders.length.times do |i|
+  santas << Santa.new(santa_genders[i], santa_ethnicities[i])
+  end
+
+#Test instance methods on current Santa instances housed in above array
+
+santas.each do |instance|
+  instance.speak
+  instance.eat_milk_and_cookies("chocolate chip")
+end
 
