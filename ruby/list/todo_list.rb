692c20f1d641d9d4b87e28dbad42e1378987c206
Brittany Snyder <snyderbc@gmail.com>
2017-08-26 12:31:03 -0500

    Complete get item method and confirm rspec produces all 4 successes

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -3,28 +3,28 @@
 # One by one, confirm that the tests are turning from red to green as each method is completed
 
 
-
 class TodoList
 
-attr_accessor :get_items
+  attr_accessor :get_items
 
-#stores the list items given on initialization
-def initialize(item_array)
-  @get_items = item_array
-end
+  #stores the list items given on initialization
+  def initialize(item_array)
+    @get_items = item_array
+  end
 
-#adds an item to the list
-def add_item(item_to_add)
-  @get_items << item_to_add
-end
+  #adds an item to the list
+  def add_item(item_to_add)
+    @get_items << item_to_add
+  end
 
-#deletes an item
-def delete_item(item_to_delete)
-  @get_items.delete(item_to_delete)
-end
+  #deletes an item
+  def delete_item(item_to_delete)
+    @get_items.delete(item_to_delete)
+  end
 
-#retrieves an item by index
-def
-
+  #retrieves an item by index
+  def get_item(index)
+    @get_items[index]
+  end
 
 end
