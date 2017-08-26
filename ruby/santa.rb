faab2af0441c48b5c49fdb9e56fa8f017a1ee090
Brittany Snyder <snyderbc@gmail.com>
2017-08-25 19:48:44 -0500

    Create program to build many, many santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,14 @@
 class Santa
 
-  attr_reader :age, :ethnicity
-  attr_accessor :gender
+  attr_reader :ethnicity, :reindeer_ranking
+  attr_accessor :gender, :age
 
   def initialize(gender, ethnicity)
-    puts "Initiaizing Santa instance..."
+    puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+    @age = rand(140)
   end
 
   def speak
@@ -42,26 +42,42 @@ end
 ########DRIVER CODE###############
 
 #Create several instances of Santa class, with diverse genders and ethnicities
-santas = []
-santa_genders = ["genderless", "pangender", "male", "queer", "male"]
-santa_ethnicities = ["hispanic", "white", "black", "native american", "asian"]
+# santas = []
+# santa_genders = ["genderless", "pangender", "male", "queer", "male"]
+# santa_ethnicities = ["hispanic", "white", "black", "native american", "asian"]
 
-santa_genders.length.times do |i|
-  santas << Santa.new(santa_genders[i], santa_ethnicities[i])
-  end
+# santa_genders.length.times do |i|
+#   santas << Santa.new(santa_genders[i], santa_ethnicities[i])
+#   end
+
 
 #Test instance methods on current Santa instances housed in above array
 
-santas.each do |instance|
-  instance.about
-  instance.speak
-  instance.eat_milk_and_cookies("chocolate chip")
-  instance.get_mad_at("Rudolph")
-  instance.celebrate_birthday
-  instance.about
-  puts "This santa's age is #{instance.age}, and ethnicity is #{instance.ethnicity}"
-  instance.gender = "female"
-  instance.about
-  puts "Now all of the santas are #{instance.gender}, mwahaha"
+# santas.each do |instance|
+#   instance.about
+#   instance.speak
+#   instance.eat_milk_and_cookies("chocolate chip")
+#   instance.get_mad_at("Rudolph")
+#   instance.celebrate_birthday
+#   instance.about
+#   puts "This santa's age is #{instance.age}, and ethnicity is #{instance.ethnicity}"
+#   instance.gender = "female"
+#   instance.about
+#   puts "Now all of the santas are #{instance.gender}, mwahaha"
+# end
+
+#Program to build many, many santas
+
+def santa_creator(number_of_santas)
+  example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+  example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+  number_of_santas.times do
+    santa_instance = Santa.new(example_genders.sample, example_ethnicities.  sample)
+    p santa_instance.gender
+    p santa_instance.age
+    p santa_instance.ethnicity
+    p santa_instance.reindeer_ranking
+    end
 end
 
+santa_creator(1000)
