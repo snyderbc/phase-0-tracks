6caf8664891d7077568f44aef3e7267ce75d429c
Brittany Snyder <snyderbc@gmail.com>
2017-08-26 08:52:12 -0500

    Add mixin module to Bird and Plane classes, and test to confirm

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,5 +1,21 @@
+#This is a mixin module, so we don't have to use "self"
+
+module Flight
+  def take_off(altitude)
+    puts "Takingoff and ascending until reaching #{altitude}  ..."
+  end
+end
+
 class Bird
+  include Flight
 end
 
 class Plane
+  include Flight
 end
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
