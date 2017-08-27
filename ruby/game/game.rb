47f88ae4b03153c7dac32fdd1cfb241fd3f11c5f
Brittany Snyder <snyderbc@gmail.com>
2017-08-27 18:21:58 -0500

    Add pseudocode for user interface, and add code to collect user 1 input and instructions for user 2

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -26,3 +26,14 @@ end
 
 
 #####DRIVER CODE ##############
+
+# Get a word from user 1 (mystery word)
+# Then, allow user 2 to input a letter until he either spells out the mystery word or runs out of guesses.
+# IF he guesses the mystery word before running out of guesses, display a congratulatory message.
+# OTHERWISE Taunt him mercilessly.
+
+puts "Welcome to the Word Game! Give me a word, and we'll see if your friend is smart enough to guess it:"
+user1_input = gets.chomp
+wordgame = WordGame.new(user1_input)
+
+puts "Thanks! Now let your friend guess a letter, one at a time. Choose wisely - you only get #{wordgame.guesses_available} guesses!"
