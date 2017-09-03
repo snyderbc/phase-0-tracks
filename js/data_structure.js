18b02fe48b17bf560b2ed11640b3d75e04e1f099
Brittany Snyder <snyderbc@gmail.com>
2017-09-03 18:07:32 -0500

    Complete release 0, create color and names arrays and an item to each

--- js/data_structure.js
+++ js/data_structure.js
@@ -1,1 +1,12 @@
-var colors = ["blue", "pink", "green", "red"]
+var colors = ["blue", "pink", "green", "red"];
+
+var names = ["Joe", "Jolene", "Jerry", "Jess"];
+
+console.log(colors);
+console.log(names);
+
+colors.push("purple");
+names.push("Jeff");
+
+console.log(colors);
+console.log(names);
