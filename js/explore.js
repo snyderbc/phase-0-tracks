da51ab2f922024d986c37b334f083bd020feaefe
Brittany Snyder <snyderbc@gmail.com>
2017-09-03 16:05:44 -0500

    Create reverse function and test it on hello

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,15 @@
-//
+//PSEUDOCODE
+
+//Take a string as a paramater
+// Run through each letter in the string, starting with the last item (at index -1) and running through the next to last time, and so on until you reach the first letter of the word. Add each letter to a new reversed word as you go.
+
+var reverseString = ""
+
+function reverse(str) {
+  for (var i = str.length; i > 0; i += -1) {
+   reverseString = reverseString + (str[i-1]);
+  }
+  console.log(reverseString);
+}
+
+reverse("hello");
