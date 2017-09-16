70e89f909f3e358938268b891cc8fc3b23d0fae8
Brittany Snyder <snyderbc@gmail.com>
2017-09-16 16:14:12 -0500

    Experiment with multiple jquery actions, adding features based on events like mouseenter, hover, mouseover, and click

--- web_dev/jquery/jquery.js
+++ web_dev/jquery/jquery.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  $("li").hide()
-})
+    $("li:first").mouseenter(function()
+      {alert("If you click on a the world link, it will turn blue!");
+      });
+
+    // $("li").click(function(){
+    //   $(this).fadeOut("slow");
+    // });
+
+    $("#first_label").hide();
+    $("[href]").mouseover(function(){
+      $("#first_label").fadeIn("slow");
+    });
+
+    $("[href]").click(function() {
+      $(this).css("background-color", "blue");
+    });
+});
